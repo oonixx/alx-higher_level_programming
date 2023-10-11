7fa2e9fbbe3bd5e5ed1e9837fb0e8aec6f35c2bb
root <imrane.achaari@yahoo.com>
2023-10-11 16:56:44 -0700

    the 1 task

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -8,5 +8,5 @@ def search_replace(my_list, search, replace):
     if len(my_list) == 0:
         return my_list
 
-    new_lst = [elm if elem != search else replace for elm in my_list]
+    new_lst = [elm if elm != search else replace for elm in my_list]
     return new_lst
